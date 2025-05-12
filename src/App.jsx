cf8cee56653ad925f201d153e364d9bdac438431
Brian Mulawka <mulawkabrian@gmail.com>
2025-05-12 11:31:02 +0200

    activitée 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import {Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home />}/>
+        </Route>
       </Routes>
     </>
   );
