e0150f9a7c17e5477a2e0b85b33295a1c7f6b36e
Brian Mulawka <mulawkabrian@gmail.com>
2025-05-12 14:15:50 +0200

    activitée 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import "./App.css";
 import Home from "./pages/Home.jsx";
 import Layout from "./components/Layout.jsx";
 import Category from "./pages/Category.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Layout/>}>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" index element={<Home />}/>
           <Route path="/category/:categoryId" element={<Category />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
