e5985e948addaa0eb7f1c4323575de535ef42f01
Brian Mulawka <mulawkabrian@gmail.com>
2025-05-13 12:25:28 +0200

    activitée 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,31 +3,24 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { NavLink } from "react-router";
 import "../styles/Header.css";
+import { useMatch } from "react-router";
 
 function Header() {
+  const match = useMatch("/category/:query");
   return (
     <>
       <header>
         <Navbar bg="light" data-bs-theme="light">
           <Container>
             <Nav className="ms-auto">
-              <Nav.Link
-                to="/"
-                as={NavLink}
-                activeClassName="selected"
-              >
+              {match ? <Navbar >Catégorie : {match.params.query}</Navbar> : ""}
+              <Nav.Link to="/" as={NavLink}>
                 Accueil
               </Nav.Link>
-              <Nav.Link
-                to="/category/vetements"
-                as={NavLink}
-              >
+              <Nav.Link to="/category/vetements" as={NavLink}>
                 Vêtements
               </Nav.Link>
-              <Nav.Link
-                to="/category/chaussures"
-                as={NavLink}
-              >
+              <Nav.Link to="/category/chaussures" as={NavLink}>
                 Chaussures
               </Nav.Link>
             </Nav>
