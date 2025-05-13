a65a3519bdd4d161ee0ded14e8ab2d43dbc5d7f3
Brian Mulawka <mulawkabrian@gmail.com>
2025-05-13 11:11:40 +0200

    activitée 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,44 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
+import "../styles/Header.css";
 
 function Header() {
   return (
     <>
-    <header>
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Nav className="ms-auto">
-            <Nav.Link to="/" as={Link}>
-              Accueil
-            </Nav.Link>
-            <Nav.Link to="/category/vetements" as={Link}>
-              Vêtements
-            </Nav.Link>
-            <Nav.Link to="/category/chaussures" as={Link}>
-              Chaussures
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </header>
+      <header>
+        <Navbar bg="light" data-bs-theme="light">
+          <Container>
+            <Nav className="ms-auto">
+              <NavLink
+                to="/"
+                className={({isActive}) =>
+                 isActive ? "selected" : "nav-link"
+                }
+              >
+                Accueil
+              </NavLink>
+              <NavLink
+                to="/category/vetements"
+                className={({isActive}) =>
+                 isActive ? "selected" : "nav-link"
+                }
+              >
+                Vêtements
+              </NavLink>
+              <NavLink
+                to="/category/chaussures"
+                className={({isActive}) =>
+                 isActive ? "selected" : "nav-link"
+                }
+              >
+                Chaussures
+              </NavLink>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
     </>
   );
 }
